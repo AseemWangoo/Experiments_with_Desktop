d26a568286b1532d6bed86213a4db3e6edb32725
aseem wangoo <aseemwangoo@gmail.com>
2020-01-19 17:52:48 +0800

    [C/SS] Rotation by z

--- first_desktop_application/lib/solarsystem/home.dart
+++ first_desktop_application/lib/solarsystem/home.dart
@@ -1,15 +1,61 @@
+import 'package:first_desktop_application/app-level/utils/screen_size.dart';
+
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(seconds: 50),
+      vsync: this,
+    );
+
+    _controller.forward().orCancel;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    //
+    Offset _offset = Offset(0.3, -0.9);
+    // timeDilation = 2.0;
+
+    final _height = ScreenQueries.instance.height(context);
+    final _width = ScreenQueries.instance.width(context);
 
     return Scaffold(
       appBar: AppBar(),
-      body: Text('HI SS'),
+      // backgroundColor: Colors.black,
+      body: Center(
+        child: Transform(
+          alignment: FractionalOffset.centerLeft,
+          transform: Matrix4.identity()
+            ..setEntry(3, 2, 0.0011)
+            ..rotateX(_offset.dy)
+            ..rotateY(_offset.dx),
+          child: RotationTransition(
+            turns: _controller,
+            child: Image.asset(
+              'assets/images/galaxy.jpg',
+              fit: BoxFit.contain,
+              height: _height,
+              width: _width,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
