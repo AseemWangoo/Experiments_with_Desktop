1a59b3d4dd5a8e56f6b7f5faf8e110995d4e2176
aseem wangoo <aseemwangoo@gmail.com>
2020-01-28 22:40:13 +0800

    [C/Inking] Content display

--- first_desktop_application/lib/inking/home.dart
+++ first_desktop_application/lib/inking/home.dart
@@ -1,3 +1,5 @@
+import 'package:first_desktop_application/inking/widgets/dark_ink_content.dart';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,9 +7,13 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //
+
     return Scaffold(
       appBar: AppBar(title: const Text('Inking')),
-      body: Text('Hiii'),
+      body: DarkInkContent(
+        darkMode: true,
+      ),
     );
   }
 }
