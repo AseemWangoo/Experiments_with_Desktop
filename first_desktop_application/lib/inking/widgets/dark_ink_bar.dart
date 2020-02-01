9b10757b9acc90ed1684fb19a5f931b6677f5709
aseem wangoo <aseemwangoo@gmail.com>
2020-02-01 21:29:58 +0800

    [C/Inking] Finalise step 1

--- first_desktop_application/lib/inking/widgets/dark_ink_bar.dart
+++ first_desktop_application/lib/inking/widgets/dark_ink_bar.dart
@@ -6,12 +6,47 @@ class DarkInkBar extends StatefulWidget {
   const DarkInkBar({Key key, this.darkModeValue}) : super(key: key);
 
   @override
-  _DarkInkBarState createState() => _DarkInkBarState();
+  _DarkInkBarState createState() => _DarkInkBarState(darkModeValue);
 }
 
-class _DarkInkBarState extends State<DarkInkBar> {
+class _DarkInkBarState extends State<DarkInkBar>
+    with SingleTickerProviderStateMixin {
   static final Color darkColor = Color(0xFF171137);
   static final Color lightColor = Color(0xFF67ECDC);
+
+  AnimationController _controller;
+  Animation<double> _iconOpacityAnimation;
+  ImageProvider _darkModeToggleIconImage;
+
+  bool darkModeValue;
+
+  _DarkInkBarState(this.darkModeValue);
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _iconOpacityAnimation = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 1.0, end: 0.0),
+        weight: .20,
+      ),
+      TweenSequenceItem(
+        tween: ConstantTween<double>(0.0),
+        weight: .20,
+      ),
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 0.0, end: 1.0),
+        weight: .20,
+      ),
+    ]).animate(_controller);
+
+    _darkModeToggleIconImage = AssetImage('assets/images/icon-moon.png');
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,16 +81,16 @@ class _DarkInkBarState extends State<DarkInkBar> {
                       'assets/images/icon-r.png',
                     )),
                   ),
-                  // FlatButton(
-                  //     onPressed: () => darkModeValue = !(darkModeValue ?? true),
-                  //     splashColor: Colors.transparent,
-                  //     highlightColor: Colors.transparent,
-                  //     textColor: foregroundColor,
-                  //     child: Opacity(
-                  //       opacity: _iconOpacityAnimation.value,
-                  //       child: ImageIcon(_darkModeToggleIconImage),
-                  //     ),
-                  //   ),
+                  FlatButton(
+                    onPressed: () => darkModeValue = !(darkModeValue ?? true),
+                    splashColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
+                    // textColor: foregroundColor,
+                    child: Opacity(
+                      opacity: _iconOpacityAnimation.value,
+                      child: ImageIcon(_darkModeToggleIconImage),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -67,5 +102,11 @@ class _DarkInkBarState extends State<DarkInkBar> {
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
   }
 }
