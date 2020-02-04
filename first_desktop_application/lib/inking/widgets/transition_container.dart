acbc26a2d417ae6aa9a8bf518223747aa9e02d59
aseem wangoo <aseemwangoo@gmail.com>
2020-02-04 22:38:29 +0800

    [C/Transition] Base for switching

--- first_desktop_application/lib/inking/widgets/transition_container.dart
+++ first_desktop_application/lib/inking/widgets/transition_container.dart
@@ -15,11 +15,69 @@ class TransitionContainer extends StatefulWidget {
   _TransitionContainerState createState() => _TransitionContainerState();
 }
 
-class _TransitionContainerState extends State<TransitionContainer> {
+class _TransitionContainerState extends State<TransitionContainer>
+    with SingleTickerProviderStateMixin {
+  //
+
+  AnimationController _controller;
+  Animation<double> _animation;
+
+  List<ImageProvider> _images;
+  int _currentImageIndex = 0;
+
+  Widget _childForeground;
+  Widget _childBackground;
+
   @override
   void initState() {
-    // TODO: implement initState
+    _images = [
+      AssetImage('assets/images/ink_mask.png'),
+      AssetImage('assets/images/wipe_mask.png'),
+      AssetImage('assets/images/tendril_mask.png'),
+    ];
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          _childBackground = _childForeground;
+          _childForeground = null;
+        });
+        _controller.reset();
+      }
+    });
+
+    _animation = TweenSequence(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 0.0),
+          weight: 30,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 34.0),
+          weight: 70,
+        ),
+      ],
+    ).animate(_controller);
+
     super.initState();
+  }
+
+  @override
+  void didUpdateWidget(TransitionContainer oldWidget) {
+    _handleDarkModeChange();
+    if (widget.child != oldWidget.child) {
+      setState(() => _childForeground = widget.child);
+      _controller.forward();
+    }
+    super.didUpdateWidget(oldWidget);
   }
 
   @override
@@ -28,6 +86,15 @@ class _TransitionContainerState extends State<TransitionContainer> {
     final width = appSize.width;
     final height = appSize.height;
 
+    print('>>>> FG ${_childForeground.runtimeType}');
+    print('>>>> BG ${_childBackground.runtimeType}');
+
+    // 0 => light mode
+    // Clicked on first time, then index goes to 1 (dark Mode)...
+    // Clicked again, index = 1 (light mode)..
+    // CLicked on dark mode, index = 2 (2 >=3)
+    print('>>>> Image ${_images[_currentImageIndex]} >>> $_currentImageIndex');
+
     List<Widget> children = <Widget>[
       Container(
         width: width,
@@ -35,6 +102,17 @@ class _TransitionContainerState extends State<TransitionContainer> {
         child: widget.child,
       ),
     ];
+
+    // If we swapped the child then add the foreground to the list of children when animating
+    if (_childForeground != null) {
+      children.add(
+        Container(
+          width: width,
+          height: height,
+          child: _childForeground,
+        ),
+      );
+    }
 
     return Positioned(
       left: 0,
@@ -48,6 +126,29 @@ class _TransitionContainerState extends State<TransitionContainer> {
 
   @override
   void dispose() {
+    _controller?.dispose();
     super.dispose();
   }
+
+  void _handleDarkModeChange() {
+    if (widget.darkModeValue) {
+      setState(() {
+        ++_currentImageIndex;
+        if (_currentImageIndex >= _images.length) {
+          _currentImageIndex = 0;
+        }
+      });
+    }
+  }
 }
+
+/*
+
+When clicked on mode change first
+flutter: >>>> BG Null
+flutter: >>>> FG DarkInkContent
+
+At the end of animation....
+flutter: >>>> FG Null
+flutter: >>>> BG DarkInkContent
+ */
