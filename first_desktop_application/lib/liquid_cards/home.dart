2ae9aa4d5372fa51a098b6fc69e51a47eb9da6f5
aseem wangoo <aseemwangoo@gmail.com>
2020-02-15 18:34:27 +0800

    [C/LC] UI

--- first_desktop_application/lib/liquid_cards/home.dart
+++ first_desktop_application/lib/liquid_cards/home.dart
@@ -1,4 +1,5 @@
 import 'package:first_desktop_application/liquid_cards/data/demo_data.dart';
+import 'package:first_desktop_application/liquid_cards/styles/styles.dart';
 import 'package:first_desktop_application/liquid_cards/widgets/drink_card.dart';
 
 import 'package:flutter/material.dart';
@@ -28,6 +29,9 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     //
+    bool isLandscape = MediaQuery.of(context).size.aspectRatio > 1;
+    double headerHeight =
+        MediaQuery.of(context).size.height * (isLandscape ? .25 : .2);
 
     return Scaffold(
       appBar: AppBar(title: Text('Liquid Cards')),
@@ -43,6 +47,57 @@ class _HomeState extends State<Home> {
               controller: _scrollController,
               itemBuilder: (context, index) => _buildListItem(index),
             ),
+            _buildTopBg(headerHeight),
+            _buildTopContent(headerHeight),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // TODO:Rounded
+  Widget _buildTopBg(double height) {
+    return Container(
+      alignment: Alignment.topCenter,
+      height: height,
+      child: Container(
+        width: double.infinity,
+        child: Image.asset(
+          "assets/images/Header-Dark.png",
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTopContent(double height) {
+    return SafeArea(
+      child: Container(
+        alignment: Alignment.topCenter,
+        height: height,
+        padding: EdgeInsets.all(height * .1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              "My Rewards",
+              style: Styles.text(height * .13, Colors.white, true),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.star, color: AppColors.redAccent, size: height * .2),
+                const SizedBox(width: 8),
+                Text(
+                  "$_earnedPoints",
+                  style: Styles.text(height * .3, Colors.white, true),
+                ),
+              ],
+            ),
+            Text(
+              "Your Points Balance",
+              style: Styles.text(height * .1, Colors.white, false),
+            )
           ],
         ),
       ),
