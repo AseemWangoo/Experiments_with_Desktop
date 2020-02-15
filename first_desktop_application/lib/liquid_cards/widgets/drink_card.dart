d7cfb62dc223b7c501c56890c5ce60a63bd1b95f
aseem wangoo <aseemwangoo@gmail.com>
2020-02-15 18:21:49 +0800

    [C/LC] Demo Data

--- first_desktop_application/lib/liquid_cards/widgets/drink_card.dart
+++ first_desktop_application/lib/liquid_cards/widgets/drink_card.dart
@@ -1,4 +1,6 @@
 import 'package:first_desktop_application/liquid_cards/data/demo_data.dart';
+import 'package:first_desktop_application/liquid_cards/styles/styles.dart';
+
 import 'package:flutter/material.dart';
 
 class DrinkListCard extends StatefulWidget {
@@ -41,6 +43,9 @@ class _DrinkListCardState extends State<DrinkListCard>
 
   @override
   Widget build(BuildContext context) {
+    //Determine the points required text value, using the _pointsTween
+    var pointsRequired = widget.drinkData.requiredPoints;
+
     return GestureDetector(
       onTap: _handleTap,
       child: Container(
@@ -55,6 +60,10 @@ class _DrinkListCardState extends State<DrinkListCard>
                 child: Column(
                   children: <Widget>[
                     const SizedBox(height: 24),
+                    _buildTopContent(),
+                    //Spacer
+                    const SizedBox(height: 12.0),
+                    _buildBottomContent(),
                   ],
                 ),
               ),
@@ -62,6 +71,63 @@ class _DrinkListCardState extends State<DrinkListCard>
           ],
         ),
       ),
+    );
+  }
+
+  Row _buildTopContent() => Row(
+        children: <Widget>[
+          //Icon
+          Image.asset(
+            "assets/images/" + widget.drinkData.iconImage,
+            fit: BoxFit.fitWidth,
+            width: 50.0,
+          ),
+          SizedBox(width: 24),
+          //Label
+          Expanded(
+            child: Text(
+              widget.drinkData.title.toUpperCase(),
+              style: Styles.text(18, Colors.white, true),
+            ),
+          ),
+          //Star Icon
+          Icon(Icons.star, size: 20, color: AppColors.orangeAccent),
+          SizedBox(width: 4),
+          //Points Text
+          Text(
+            "${widget.drinkData.requiredPoints}",
+            style: Styles.text(20, Colors.white, true),
+          )
+        ],
+      );
+
+  // TODO Add param
+  Column _buildBottomContent() {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 16),
+        Text(
+          "Redeem your points for a cup of happiness! Our signature espresso is blanced with steamed milk and topped with a light layer of foam. ",
+          textAlign: TextAlign.center,
+          style: Styles.text(14, Colors.white, false, height: 1.5),
+        ),
+        const SizedBox(height: 16.0),
+        //Main Button
+        //Main Button
+        ButtonTheme(
+          minWidth: 200,
+          height: 40,
+          child: FlatButton(
+            onPressed: () {},
+            color: AppColors.orangeAccent,
+            disabledColor: AppColors.orangeAccent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Text("REDEEM", style: Styles.text(16, Colors.white, true)),
+          ),
+        )
+      ],
     );
   }
 
