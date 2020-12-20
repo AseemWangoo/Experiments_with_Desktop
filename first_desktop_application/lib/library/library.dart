de6655af8705c7af8af0b6c2c26c85c5569154e1
aseem wangoo <aseemwangoo@gmail.com>
2020-12-20 16:41:55 +0800

    [C/Terminal] Working from CLI

--- first_desktop_application/lib/library/library.dart
+++ first_desktop_application/lib/library/library.dart
@@ -3,8 +3,16 @@ import 'dart:ffi' as ffi;
 typedef hello_world_func = ffi.Void Function();
 // Dart type definition for calling the C foreign function
 typedef HelloWorld = void Function();
+
+// C header typedef:
+typedef SystemC = ffi.Void Function();
+
+// Dart header typedef
+typedef SystemDart = void Function();
+
 void main() {
-  LibraryTest().openFromCLI();
+  // LibraryTest().openFromCLI();
+  LibraryTest().filesFromCLI();
 }
 
 class LibraryTest {
@@ -21,10 +29,22 @@ class LibraryTest {
   void openFromCLI() {
     final sysLib = ffi.DynamicLibrary.open('./dylib/libhello.dylib');
 
-    final HelloWorld hello = sysLib
-        .lookup<ffi.NativeFunction<hello_world_func>>('hello_world')
-        .asFunction();
+    final hello = sysLib.lookupFunction<SystemC, SystemDart>('hello_world');
+
+    // final HelloWorld hello = sysLib
+    //     .lookup<ffi.NativeFunction<hello_world_func>>('hello_world')
+    //     .asFunction();
+
     // Call the function
     hello();
+  }
+
+  void filesFromCLI() {
+    final sysLib = ffi.DynamicLibrary.open('./dylib/libfetchtemp.dylib');
+
+    final tempFiles =
+        sysLib.lookupFunction<SystemC, SystemDart>('fetchtemp_files');
+    // Call the function
+    tempFiles();
   }
 }
